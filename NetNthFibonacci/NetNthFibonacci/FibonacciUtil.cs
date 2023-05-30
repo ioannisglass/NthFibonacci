d593ed6ede7de6a78e70e600615a639516be43e2
SU Glass <suglass1128@gmail.com>
2023-05-30 12:09:22 -0400

    initialize to get fibonacci number with C#

---
+++ NetNthFibonacci/NetNthFibonacci/FibonacciUtil.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetNthFibonacci
+{
+    public static class FibonacciUtil
+    {
+        public static void fastDoubling(int n, int[] res)
+        {
+            int a, b, c, d;
+            int MOD = 1000000007;
+
+            // Base Condition
+            if (n == 0)
+            {
+                res[0] = 0;
+                res[1] = 1;
+                return;
+            }
+            fastDoubling((n / 2), res);
+
+            // Here a = F(n)
+            a = res[0];
+
+            // Here b = F(n+1)
+            b = res[1];
+
+            c = 2 * b - a;
+
+            if (c < 0)
+                c += MOD;
+
+            // As F(2n) = F(n)[2F(n+1) – F(n)]
+            // Here c = F(2n)
+            c = (a * c) % MOD;
+
+            // As F(2n + 1) = F(n)^2 + F(n+1)^2
+            // Here d = F(2n + 1)
+            d = (a * a + b * b) % MOD;
+
+            // Check if N is odd
+            // or even
+            if (n % 2 == 0)
+            {
+                res[0] = c;
+                res[1] = d;
+            }
+            else
+            {
+                res[0] = d;
+                res[1] = c + d;
+            }
+        }
+    }
+}
---
+++ NetNthFibonacci/NetNthFibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NetNthFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input index: ");
+            string w_strN = Console.ReadLine();
+            int w_nN = 0;
+            bool w_bParseRes = int.TryParse(w_strN.Trim(), out w_nN);
+            if (w_bParseRes == false)
+            {
+                Console.WriteLine("Input valid number.");
+                // Console.WriteLine("Input exit to terminate.");
+                // string w_strTemp = Console.ReadLine();
+                // if (w_strTemp == "exit")
+                //     Environment.Exit(0);
+                Environment.Exit(0);
+            }
+
+            int[] w_arrnRet = new int[2];
+            FibonacciUtil.fastDoubling(w_nN, w_arrnRet);
+            Console.WriteLine($"{w_nN}th of Finobacci series is {w_arrnRet[0]}.");
+            Console.ReadLine();
+        }
+    }
+}
